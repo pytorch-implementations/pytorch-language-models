ae2888b84d66889501633225626293c5222a7d7d
bentrevett <bentrevett@gmail.com>
2020-09-09 00:59:55 +0100

    update example main.py for new api decision

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import argparse
 #our modules
 import tokenizer
 import data
+import datasets
 import vocab
-import config
-import models
-import runner
+
 
 parser = argparse.ArgumentParser(description="PyTorch Language Models")
 parser.add_argument("--tokenize_fn", type=str, default="punkt")
@@ -24,8 +23,6 @@ parser.add_argument("--pad_token", type=str, default="<pad>")
 parser.add_argument("--special_tokens", nargs="*", default=None)
 parser.add_argument("--batch_size", type=int, default=128)
 parser.add_argument("--sequence_length", type=int, default=100)
-parser.add_argument("--model_config_path", type=str, default="configs/default_model.json")
-parser.add_argument("--runner_config_path", type=str, default="configs/default_runner.json")
 args = parser.parse_args()
 
 # get tokenizer
@@ -48,34 +45,3 @@ dataset = data.LanguageModelingDataset(args.train_data_path,
 
 # get data iterators
 train_iterator, valid_iterator, test_iterator = dataset.load_iterators(args.batch_size, args.sequence_length)
-
-# load model config
-model_config = config.load_model_config(args.model_config_path)
-
-# get model
-model = models.load_model(dataset,
-                          model_config)
-
-# load runner config
-runner_config = config.load_runner_config(args.runner_config_path)
-
-# get runner
-runner = runner.Runner(model,
-                       runner_config)
-
-# run training
-while runner.run():
-
-    # train model on training data
-    runner.train(train_iterator)
-    # evaluate model on validation data
-    runner.eval(valid_iterator)
-
-# evaluate model on test data
-runner.eval(test_iterator)
-
-# save tokenizer, vocab, model and runner
-dataset.tokenizer.save(runner_config['run_path'])
-dataset.vocab.save(runner_config['run_path'])
-model.save(runner_config['run_path'])
-runner.save(runner_config['run_path'])
